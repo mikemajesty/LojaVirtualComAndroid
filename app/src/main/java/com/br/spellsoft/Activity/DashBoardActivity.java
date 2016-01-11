5adab51b5271032203a5b39c26b78774195b54a9
mikemajesty <mikemajesty2011@gmail.com>
2016-01-10 23:58:29 -0200

    Creating dashboard screen

---
+++ app/src/main/java/com/br/spellsoft/Activity/DashBoardActivity.java
@@ -0,0 +1,46 @@
+package com.br.spellsoft.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.br.spellsoft.Activity.R;
+
+public class DashBoardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dash_board, menu);
+        return true;
+    }
+    public void Btn_Cadastro_Click(View v){
+        startActivity(new Intent(this,PessoaActivity.class));
+    }
+    public void Btn_Listar_Click(View v){
+        startActivity(new Intent(this,ListaPessoaActivity.class));
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
